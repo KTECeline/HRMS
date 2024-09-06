01d23ddf44964ab6ed654ebfb88c8ba0b6b95dd0
Khaw Tze Ern,Celine <157878271+KTECeline@users.noreply.github.com>
2024-09-06 21:51:39 +0800

    Update txt

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        auth.LoginForm Login = new auth.LoginForm();
+       Login.setVisible(true);
+         
+       
     }
     
 }
